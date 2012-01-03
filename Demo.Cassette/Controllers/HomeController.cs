88f9808498bb0f4c3898fa5f1568264e996a8158
andrey.chumak <andrexx@gmail.com>
2012-01-03 21:51:41 +0200

    add demo project for Cassette library

---
+++ Demo.Cassette/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Demo.Cassette.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+}
